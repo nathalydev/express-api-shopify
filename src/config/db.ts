93a4d446f1458b717626e1419313545c51ad80b0
nathalydev <andreinanathalydev@gmail.com>
2024-05-21 12:46:03 -0400

    Update db.ts
    update config database

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,9 +1,14 @@
 import pg from 'pg';
-
+import dotenv from "dotenv";
+dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL
-  })
+    connectionString: process.env.POSTGRES_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+  },
+  )
 
 export default pool;
